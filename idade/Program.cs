986d7684e0e6b184d861955a32aa2cc94d69a246
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-11-12 16:59:20 +0000

    organiza codigo

--- idade/Program.cs
+++ idade/Program.cs
@@ -2,35 +2,28 @@
 using System.Text.RegularExpressions;
 
 var Jekyll = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent!.GetDirectories("_jekyll")[0];
-
 var regex = new Regex(@"<code.*>\[IDADE\]:([\d]{4}\-[\d]{2}\-[\d]{2})\<\/code>", RegexOptions.Multiline);
-var today = DateTime.Today;
+
 foreach (var file in Jekyll!.GetFiles("*.html", new EnumerationOptions() { RecurseSubdirectories = true }))
 {
-    var @new = new FileInfo(file.FullName.Replace("/_jekyll/", "/_site/"));
-
-    if (!@new.Directory!.Exists)
-        @new.Directory.Create();
-
     using var fileStrem = file.OpenText();
     var content = fileStrem.ReadToEnd();
     do
     {
         var match = regex.Match(content);
-
-        Console.WriteLine("find:" + regex.ToString() + "->" + match.Success);
         if (!match.Success)
             break;
-        Console.WriteLine("TODAY:" + today);
-        Console.WriteLine("MATCH:" + match.Groups[1].Value);
-
-        var age = AgeCalculator.Calculate(DateTime.ParseExact(match.Groups[1].Value, "yyyy-mm-dd", CultureInfo.InvariantCulture));
-
-        Console.WriteLine("AGE:" + age);
-
-        content = content.Replace(match.Groups[0].Value, age.ToString());
+        content = content.Replace(
+            match.Groups[0].Value, 
+            AgeCalculator.Calculate(DateTime.ParseExact(match.Groups[1].Value, "yyyy-mm-dd", CultureInfo.InvariantCulture)).ToString()
+        );
 
     } while (true);
+
+    var @new = new FileInfo(file.FullName.Replace("/_jekyll/", "/_site/"));
+
+    if (!@new.Directory!.Exists)
+        @new.Directory.Create();
 
     using var writer = @new.CreateText();
     writer.Write(content);
