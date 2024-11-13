575bcd57ff3f0f754be05821c325daff7acb7e69
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-11-13 01:58:28 +0000

    traz o calculo da idade

--- blockquote/Mediatr.cs
+++ blockquote/Mediatr.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using HtmlAgilityPack;
 using MediatR;
@@ -31,13 +33,13 @@ internal sealed class HtmlFileGetStreamHandler : IStreamRequestHandler<HtmlFileG
     }
 }
 
-internal sealed class BlockquoteFormatterRequest : IRequest<string?>
+internal sealed class BlockquoteFormatRequest : IRequest<string?>
 {
     public FileInfo? FileInfo { get; init; }
 }
-internal sealed class BlockquoteFormatterRequestHandler : IRequestHandler<BlockquoteFormatterRequest, string?>
+internal sealed class BlockquoteFormatRequestHandler : IRequestHandler<BlockquoteFormatRequest, string?>
 {
-    public async Task<string?> Handle(BlockquoteFormatterRequest request, CancellationToken cancellationToken)
+    public async Task<string?> Handle(BlockquoteFormatRequest request, CancellationToken cancellationToken)
     {
         using var stream = request.FileInfo!.OpenRead();
         await Task.Yield();
@@ -143,65 +145,102 @@ internal sealed class BlockquoteFormatterRequestHandler : IRequestHandler<Blockq
     }
 }
 
-internal sealed class SvgFormatterRequest : IRequest<string?>
+internal sealed class SvgFormatRequest : IRequest<string?>
 {
     public string? Content { get; init; }
 }
-internal sealed class SvgFormatterRequestHandler : IRequestHandler<SvgFormatterRequest, string?>
+internal sealed class SvgFormatRequestHandler : IRequestHandler<SvgFormatRequest, string?>
 {
-    public async Task<string?> Handle(SvgFormatterRequest request, CancellationToken cancellationToken)
+    public async Task<string?> Handle(SvgFormatRequest request, CancellationToken cancellationToken)
     {
-        return SvgFormatter.Format(request.Content);
+        if (request.Content == default)
+            return default;
+
+        return request
+            .Content
+            .Replace(
+                "[!NOTE]",
+                SvgCreate("#1f6feb", "M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"))
+            .Replace(
+                "[!TIP]",
+                SvgCreate("#3fb950", "M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.75 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"))
+            .Replace(
+                "[!IMPORTANT]",
+                SvgCreate("#ab7df8", "M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"))
+            .Replace(
+                "[!WARNING]",
+                SvgCreate("#d29922", "M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"))
+            .Replace(
+                "[!CAUTION]",
+                SvgCreate("#f85149", "M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z")
+            );
     }
 
-    static class SvgFormatter
+    static string SvgCreate(string color, string shape)
     {
-        internal static string? Format(string? content)
-        {
-            if (content == default)
-                return default;
+        var namespaceUri = "http://www.w3.org/2000/svg";
+        var svgElement = new XElement(XName.Get("svg", namespaceUri),
+            new XAttribute("viewBox", "0 0 16 16"),
+            new XAttribute("version", "1.1"),
+            new XAttribute("width", "16"),
+            new XAttribute("height", "16"),
+            new XAttribute("aria-hidden", "true"),
+            new XElement(XName.Get("path", namespaceUri),
+                new XAttribute("d", shape),
+                new XAttribute("style", $"fill:{color}")
+            )
+        );
 
-            var noteKey = "[!NOTE]";
-            var noteHtml = SvgCreate("#1f6feb", "M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z");
-
-            var tipKey = "[!TIP]";
-            var tipHtml = SvgCreate("#3fb950", "M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.75 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z");
-
-            var importantKey = "[!IMPORTANT]";
-            var importantHtml = SvgCreate("#ab7df8", "M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z");
-
-            var warningKey = "[!WARNING]";
-            var warningHtml = SvgCreate("#d29922", "M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z");
-
-            var cautionKey = "[!CAUTION]";
-            var cautionHtml = SvgCreate("#f85149", "M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z");
-            return content.Replace(noteKey, noteHtml)
-                            .Replace(tipKey, tipHtml)
-                            .Replace(importantKey, importantHtml)
-                            .Replace(warningKey, warningHtml)
-                            .Replace(cautionKey, cautionHtml);
-        }
-
-        static string SvgCreate(string color, string shape)
-        {
-            var namespaceUri = "http://www.w3.org/2000/svg";
-            var svgElement = new XElement(XName.Get("svg", namespaceUri),
-                new XAttribute("viewBox", "0 0 16 16"),
-                new XAttribute("version", "1.1"),
-                new XAttribute("width", "16"),
-                new XAttribute("height", "16"),
-                new XAttribute("aria-hidden", "true"),
-                new XElement(XName.Get("path", namespaceUri),
-                    new XAttribute("d", shape),
-                    new XAttribute("style", $"fill:{color}")
-                )
-            );
-
-            return svgElement.ToString();
-        }
+        return svgElement.ToString();
     }
 }
 
+
+internal sealed class IdadeBuildRequest : IRequest<string?>
+{
+    public string? Content { get; init; }
+}
+internal sealed class IdadeBuildRequestHandler : IRequestHandler<IdadeBuildRequest, string?>
+{
+    Regex Regex { get; }
+    public IdadeBuildRequestHandler()
+    {
+        Regex = new Regex(@"<code.*>\[IDADE\]:([\d]{4}\-[\d]{2}\-[\d]{2})\<\/code>", RegexOptions.Multiline);
+    }
+    public async Task<string?> Handle(IdadeBuildRequest request, CancellationToken cancellationToken)
+    {
+        await Task.Yield();
+        if (request.Content == default)
+            return default;
+
+        var content = request.Content;
+        do
+        {
+            var match = Regex.Match(content);
+            if (!match.Success)
+                break;
+            content = content.Replace(
+                match.Groups[0].Value,
+                Calculate(DateTime.ParseExact(match.Groups[1].Value, "yyyy-mm-dd", CultureInfo.InvariantCulture)).ToString()
+            );
+
+        } while (true);
+
+        return content;
+    }
+
+    internal static int Calculate(DateTime birthDate)
+    {
+        DateTime today = DateTime.Today;
+
+        int age = today.Year - birthDate.Year;
+
+        if (birthDate.Date > today.AddYears(-age).Date)
+            return age - 1;
+
+        return age;
+    }
+}
 
 internal sealed class BuildRequest : IRequest
 {
@@ -216,8 +255,9 @@ internal sealed class BuildRequestHandler(IMediator mediator) : IRequestHandler<
         if (!site.Directory!.Exists)
             site.Directory.Create();
 
-        var content = await mediator.Send(new BlockquoteFormatterRequest { FileInfo = request.FileInfo }, cancellationToken);
-        content = await mediator.Send(new SvgFormatterRequest { Content = content }, cancellationToken);
+        var content = await mediator.Send(new BlockquoteFormatRequest { FileInfo = request.FileInfo }, cancellationToken);
+        content = await mediator.Send(new SvgFormatRequest { Content = content }, cancellationToken);
+        content = await mediator.Send(new IdadeBuildRequest { Content = content }, cancellationToken);
 
         using var writer = site.CreateText();
         await writer.WriteAsync(content);
